af55d51b4f8b76e65a2027f7544f18319d9dabcc
Jonathan Keller <keller.jonathan@gmail.com>
2017-11-02 22:13:00 -0700

    adding directory checks

--- commands/mlog-init.js
+++ commands/mlog-init.js
@@ -15,6 +15,25 @@ LOG(commander);
 
 const logLocation = commander.args[0];
 
+if (fs.existsSync(logLocation)) {
+  if (!fs.statSync(logLocation).isDirectory()) {
+    throw new Error(`${logLocation} already exists and is not a directory.`);
+  }
+} else {
+  const parentDir = path.dirname(logLocation);
+  LOG(`Path does not exist, checking existence of ${parentDir}`);
+  if (!fs.existsSync(parentDir)) {
+    throw new Error(`${parentDir} does not exist.`);
+  }
+  try {
+    // eslint-disable-next-line no-bitwise
+    fs.accessSync(parentDir, fs.constants.R_OK | fs.constants.W_OK);
+  } catch (e) {
+    throw new Error(`${parentDir} does not have r/w access.`);
+  }
+  fs.mkdirSync(logLocation);
+}
+
 const configFileLocation = `${os.homedir()}/.mlog-config`;
 const config = { mlogLocation: logLocation };
 
